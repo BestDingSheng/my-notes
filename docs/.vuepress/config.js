19a630f819cc8b229728b2f2300cb5064bb28bf6
dingsheng <7270404141@qq.com>
2020-11-03 11:48:29 +0800

    chore: 修改 .travis.yml 文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,20 @@ module.exports = {
   dest: 'dist',
   themeConfig: {
     logo: '/img/logo.png',
-     nav: [
+    nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
     ],
     sidebar: [
       {
-        title: 'Group 1', // 必要的
+        title: '前端', // 必要的
+        path: '/home', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ['/home/one', '/home/tow'],
+      },
+      {
+        title: '读书', // 必要的
         path: '/foo/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1

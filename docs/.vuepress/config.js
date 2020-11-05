538a3a809ae80a95b1c3090223a83b264a1bb1cc
dingsheng <727040141@qq.com>
2020-11-05 16:21:08 +0800

    chore: change config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,56 +5,6 @@ module.exports = {
   // base: '/my-notes/',
   dest: 'dist',
   themeConfig: {
-    plugins: [
-      '@vuepress/back-to-top',
-      [
-        '@vuepress/pwa',
-        {
-          serviceWorker: true,
-          updatePopup: {
-            message: '发现新内容',
-            buttonText: '刷新',
-          },
-        },
-      ],
-      [
-        '@vuepress/last-updated',
-        {
-          dateOptions: {
-            hour12: false,
-          },
-        },
-      ],
-    ],
-    head: [
-      ['link', { rel: 'icon', href: '/logo.png' }],
-      ['link', { rel: 'manifest', href: '/manifest.json' }],
-      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-      [
-        'meta',
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-      ],
-      ['link', { rel: 'apple-touch-icon', href: '/icons/128.png' }],
-      [
-        'link',
-        {
-          rel: 'mask-icon',
-          href: '/icons/safari-pinned-tab.svg',
-          color: '#3eaf7c',
-        },
-      ],
-      ['meta', { name: 'msapplication-TileImage', content: '/icons/128.png' }],
-      ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-      [
-        'meta',
-        {
-          name: 'viewport',
-          content:
-            'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover',
-        },
-      ],
-    ],
     logo: '/img/logo.png',
     nav: [
       { text: 'Home', link: '/' },
@@ -91,4 +41,54 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    '@vuepress/back-to-top',
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: '发现新内容',
+          buttonText: '刷新',
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated',
+      {
+        dateOptions: {
+          hour12: false,
+        },
+      },
+    ],
+  ],
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/128.png' }],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
+    ],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/128.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content:
+          'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover',
+      },
+    ],
+  ],
 };
